5ed47cfa7efbbaefa77c311583e0bc96058c23bf
rojascruz <jrojasj73@gmail.com>
2026-08-13 22:34:29 -0400

    Configure Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
   ],
+
+  base: '/Cable-Line-Communication-React/',
 })
